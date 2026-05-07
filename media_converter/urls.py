0441dbc49f928dfb29d8eb20b67c48055f312dc1
Obinna Akpulonu <akpulonujoseph@gmail.com>
2026-05-07 12:04:21 +0100

    updated root urls

--- media_converter/urls.py
+++ media_converter/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from converter.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('convert/', ConvertVideo.as_view(), name='convert-video'),
     path('get-video-info/', VideoInfo.as_view(), name='get-video-info'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
