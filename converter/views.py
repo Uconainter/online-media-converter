50069723a700135303fb9865ff96215369eca33b
Uconainter <uconainteriors@gmail.com>
2026-06-30 15:45:25 +0100

    Update views.py

--- converter/views.py
+++ converter/views.py
@@ -7,6 +7,11 @@ import yt_dlp
 from django.utils.text import slugify
 
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+YDL_OPTS = {
+    "cookiefile": os.path.join(BASE_DIR, "cookies.txt"),
+}
 
 
 class VideoInfo(APIView):
@@ -16,7 +21,7 @@ class VideoInfo(APIView):
             return Response({'error': 'Video URL is required'}, status=status.HTTP_400_BAD_REQUEST)
         
         try:
-            with yt_dlp.YoutubeDL() as ydl:
+            with yt_dlp.YoutubeDL(YDL_OPTS) as ydl:
                 info = ydl.extract_info(url, download=False)
                 video_info = {
                     'title': info.get('title', 'Unknown Title'),
@@ -38,7 +43,7 @@ class ConvertVideo(APIView):
         
         try:
             # extract the video from Youtube
-            with yt_dlp.YoutubeDL() as ydl:
+            with yt_dlp.YoutubeDL(YDL_OPTS) as ydl:
                 info = ydl.extract_info(url, download=False)
                 title = info.get('title', 'Unknown Title')
         except Exception as e:
@@ -87,5 +92,5 @@ def download_video(url, output_path, format):
         'outtmpl': output_path,
         'noplaylist': True
     }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    with yt_dlp.YoutubeDL(YDL_OPTS) as ydl:
         ydl.download([url])
